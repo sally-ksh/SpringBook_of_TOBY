050f5c0707622c06fd7eea32f23e4fbd495fd301
sally-ksh <sally.devz@gmail.com>
2022-11-02 11:12:20 +0900

    Build & Refactor #4 ApplicationContext
    - spring-boot-starter-web
    - ApplicationContext with DaoFactory
    - registered Bean - UserDao, DaoFactory

--- src/main/java/ch01/ioc/DaoFactory.java
+++ src/main/java/ch01/ioc/DaoFactory.java
@@ -1,11 +1,17 @@
 package ch01.ioc;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(getConnectionMaker());
 	}
 
-	private ConnectionMaker getConnectionMaker() {
+	@Bean
+	public ConnectionMaker getConnectionMaker() {
 		return new NConnectionMaker();
 	}
 }
--- src/main/java/ch01/ioc/UserDaoTest.java
+++ src/main/java/ch01/ioc/UserDaoTest.java
@@ -1,10 +1,15 @@
 package ch01.ioc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		UserDao userDao = new DaoFactory().userDao();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
+		// UserDao userDao = new DaoFactory().userDao();
 		User user = new User("sally", "sallysh", "study");
 		System.out.println("registered : " + user.getId());
 
