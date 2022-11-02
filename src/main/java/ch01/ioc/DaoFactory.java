3e99cd39764f48b3c9360b44e3b549a60a623a02
sally-ksh <sally.devz@gmail.com>
2022-11-02 10:37:47 +0900

    Refactor #4 DaoFactory - getConnectionMaker()
    - Dao 추가시 ConnectionMaker 중복 제거

--- src/main/java/ch01/ioc/DaoFactory.java
+++ src/main/java/ch01/ioc/DaoFactory.java
@@ -2,8 +2,10 @@ package ch01.ioc;
 
 public class DaoFactory {
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new NConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao(getConnectionMaker());
+	}
+
+	private ConnectionMaker getConnectionMaker() {
+		return new NConnectionMaker();
 	}
 }
