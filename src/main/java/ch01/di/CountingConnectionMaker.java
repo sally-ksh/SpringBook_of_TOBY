fb98ec8292b46e22b55182feeb1c2cb419d907b5
sally-ksh <sally.devz@gmail.com>
2022-11-03 11:10:27 +0900

    Add #6 CountingConnectionMaker
    - implement ConnectionMaker
    - add bean realConnectionMaker

---
+++ src/main/java/ch01/di/CountingConnectionMaker.java
@@ -0,0 +1,27 @@
+package ch01.di;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+	int counter = 0;
+	private ConnectionMaker realConnectionMaker;
+
+	public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+		this.realConnectionMaker = realConnectionMaker;
+	}
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		this.addCounter();
+		return realConnectionMaker.makeConnection();
+	}
+
+	public int getCounter() {
+		return this.counter;
+	}
+
+	private void addCounter() {
+		this.counter++;
+	}
+}
---
+++ src/main/java/ch01/di/DaoFactory.java
@@ -0,0 +1,22 @@
+package ch01.di;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+	@Bean
+	public UserDao userDao() {
+		return UserDao.getInstance(connectionMaker());
+	}
+
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new CountingConnectionMaker(realConnectionMaker());
+	}
+
+	@Bean
+	public ConnectionMaker realConnectionMaker() {
+		return new NConnectionMaker();
+	}
+}
