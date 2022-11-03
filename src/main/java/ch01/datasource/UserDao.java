e5c0e48ed26e57173eaabe15241e0abbf3e11ab9
sally-ksh <sally.devz@gmail.com>
2022-11-03 12:03:08 +0900

    Add #7 DataSource - java configuration

---
+++ src/main/java/ch01/datasource/UserDao.java
@@ -0,0 +1,56 @@
+package ch01.datasource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class UserDao {
+	private static volatile UserDao INSTANCE;
+	private DataSource dataSource;
+
+	public UserDao(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection connection = dataSource.getConnection();
+
+		PreparedStatement prepareStatement = connection.prepareStatement(
+			"insert into users(id,name, password) values (?,?,?)"
+		);
+		prepareStatement.setString(1, user.getId());
+		prepareStatement.setString(2, user.getName());
+		prepareStatement.setString(3, user.getPassword());
+
+		prepareStatement.executeUpdate();
+
+		prepareStatement.close();
+		connection.close();
+	}
+
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection connection = dataSource.getConnection();
+
+		PreparedStatement prepareStatement = connection.prepareStatement(
+			"select id, name, password from users where id = ?"
+		);
+		prepareStatement.setString(1, id);
+
+		ResultSet resultSet = prepareStatement.executeQuery();
+		resultSet.next();
+			User user = new User(
+			resultSet.getString("id"),
+			resultSet.getString("name"),
+			resultSet.getString("password")
+		);
+
+		resultSet.close();
+		prepareStatement.close();
+		connection.close();
+
+		return user;
+	}
+}
