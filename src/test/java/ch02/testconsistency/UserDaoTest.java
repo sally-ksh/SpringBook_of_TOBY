a011f6920df92de52c96fb1e957625c57a3d133b
sally-ksh <sally.devz@gmail.com>
2022-11-04 11:46:16 +0900

    Refactor #8 beforeEach and fixture

--- src/test/java/ch02/testconsistency/UserDaoTest.java
+++ src/test/java/ch02/testconsistency/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -11,38 +12,38 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import java.sql.SQLException;
 
 class UserDaoTest {
+	private UserDao dao;
+	private User userA;  // fixture
+	private User userB;
+
+	@BeforeEach
+	void beforeEach() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		dao = context.getBean("userDao", UserDao.class);
+		userA = new User("sally", "sallysh", "study");
+		userB = new User("sally2", "sallysh2", "study2");
+	}
 
 	@Test
 	void addAndGet() throws SQLException, ClassNotFoundException {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		User expectedA = new User("sally", "sallysh", "study");
-		User expectedB = new User("sally2", "sallysh2", "study2");
-
 		dao.deleteAll();
 		assertThat(dao.getCount()).isZero();
 
-		dao.add(expectedA);
-		dao.add(expectedB);
+		dao.add(userA);
+		dao.add(userB);
 		assertThat(dao.getCount()).isGreaterThan(1);
 
-		User actualA = dao.get(expectedA.getId());
+		User actualA = dao.get(userB.getId());
 		assertThat(actualA.getName()).isEqualTo(actualA.getName());
 		assertThat(actualA.getPassword()).isEqualTo(actualA.getPassword());
 
-		User actualB = dao.get(expectedB.getId());
+		User actualB = dao.get(userB.getId());
 		assertThat(actualB.getName()).isEqualTo(actualB.getName());
 		assertThat(actualB.getPassword()).isEqualTo(actualB.getPassword());
 	}
 
 	@Test
 	void getUserFailure() throws SQLException, ClassNotFoundException {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		User expected = new User("sally", "sallysh", "study");
-
 		dao.deleteAll();
 		assertThat(dao.getCount()).isZero();
 
