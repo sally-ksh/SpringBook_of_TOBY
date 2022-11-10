0d3308d149399bd4931a89403fdd8a9617a934a0
sally-ksh <sally.devz@gmail.com>
2022-11-10 21:49:20 +0900

    Setting #10 for separate class and DI

---
+++ src/main/java/ch03/jdbccontext/UserDaoGet.java
@@ -0,0 +1,14 @@
+package ch03.jdbccontext;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDaoGet implements StatementStrategy {
+	@Override
+	public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+		return connection.prepareStatement(
+			"select id, name, password from users where id = ?"
+		);
+	}
+}
