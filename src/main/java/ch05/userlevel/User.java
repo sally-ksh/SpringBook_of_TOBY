e3a87b4edc1bde4216d577043f9f0625f8bd8b30
sally-ksh <sally.devz@gmail.com>
2022-11-15 21:13:47 +0900

    Add #13 User level
    - start chapter 5

---
+++ src/main/java/ch05/userlevel/User.java
@@ -0,0 +1,55 @@
+package ch05.userlevel;
+
+import java.util.Objects;
+
+public class User {
+	private String id;
+	private String name;
+	private String password;
+
+	public User(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User)o;
+		return Objects.equals(id, user.id) && Objects.equals(name, user.name)
+			&& Objects.equals(password, user.password);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, password);
+	}
+}
