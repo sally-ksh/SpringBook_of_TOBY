1482ff92770e5e0d41d41e95356c6dc5f91b9309
sally-ksh <sally.devz@gmail.com>
2022-11-16 23:50:16 +0900

    Add and Refactor #14 SpringGlobalTransaction and PlatformTransactionManager DI

---
+++ src/main/java/ch05/transaction/springglobaltransaction/DaoFactory.java
@@ -0,0 +1,48 @@
+package ch05.transaction.springglobaltransaction;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+import ch05.transaction.BasicUserLevelUpgradePolicy;
+import ch05.transaction.UserDao;
+import ch05.transaction.UserDaoJdbc;
+import ch05.transaction.UserLevelUpgradePolicy;
+
+@Configuration
+public class DaoFactory {
+	@Bean
+	public UserService userService() {
+		return new UserService(userDao(), userLevelUpgradePolicy(), dataSource(), transactionManager());
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		// return new JtaTransactionManager();  // 글로벌 트랜잭션
+		return new DataSourceTransactionManager(dataSource());
+	}
+
+	@Bean
+	public UserLevelUpgradePolicy userLevelUpgradePolicy() {
+		return new BasicUserLevelUpgradePolicy();
+	}
+
+	@Bean
+	public UserDao userDao() {
+		return new UserDaoJdbc(dataSource());
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/database-name");
+		dataSource.setUsername("id");
+		dataSource.setPassword("password");
+		return dataSource;
+	}
+}
---
+++ src/main/java/ch05/transaction/springglobaltransaction/UserService.java
@@ -0,0 +1,60 @@
+package ch05.transaction.springglobaltransaction;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.sql.DataSource;
+
+import ch05.transaction.Level;
+import ch05.transaction.User;
+import ch05.transaction.UserDao;
+import ch05.transaction.UserLevelUpgradePolicy;
+
+public class UserService {
+	public static final int MIN_LOG_COUNT_FOR_SILVER = 50;
+	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+
+	private final DataSource dataSource;
+	private final PlatformTransactionManager transactionManager;
+	private final UserDao userDao;
+	private final UserLevelUpgradePolicy userLevelUpgradePolicy;
+
+
+	public UserService(UserDao userDao, UserLevelUpgradePolicy userLevelUpgradePolicy, DataSource dataSource, PlatformTransactionManager transactionManager) {
+		this.userDao = userDao;
+		this.userLevelUpgradePolicy = userLevelUpgradePolicy;
+		this.dataSource = dataSource;
+		this.transactionManager = transactionManager;
+	}
+
+	public void upgradeLevels() {
+		TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+		try {
+			List<User> users = userDao.getAll();
+
+			for (User user : users) {
+				if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
+					userLevelUpgradePolicy.upgradeLevel(user);
+					userDao.update(user);
+				}
+			}
+			transactionManager.commit(transactionStatus);
+		} catch (RuntimeException exception) {
+			transactionManager.rollback(transactionStatus);
+			throw exception;
+		}
+	}
+
+	public void add(User user) {
+		if (Objects.isNull(user.getLevel())) {
+			user.setLevel(Level.BASIC);
+		}
+		userDao.add(user);
+	}
+}
